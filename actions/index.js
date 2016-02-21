a46b4301aa8a95492d6a28da01f337b2af7c8a49
Mikko Suniala <mikko.suniala@iki.fi>
2016-02-21 12:47:54 +0200

    Cleanup: Remove Counter

--- actions/index.js
+++ actions/index.js
@@ -1,7 +1,3 @@
-export const increase = () => {
-    return {type: 'increase'};
-};
-
 export const placePebble = (pebble) => {
     return {
         type: 'placePebble',
--- reducers/index.js
+++ reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux'
-import count from './counter'
 import slot from './slot'
 import turn from './turn'
 
 const gameApp = combineReducers(
         {
-            count,
             slot,
             turn
         }
